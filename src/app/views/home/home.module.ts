81abd5e0d706ca81720768f5208b88161cf804b6
jramirez <jose.ramirez@siigroup.cl>
2021-04-01 21:39:56 -0300

    FEATURE: Header component and styles details

--- src/app/views/home/home.module.ts
+++ src/app/views/home/home.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { ComponentsModule } from '../../components/components.module';
 
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    ComponentsModule
   ]
 })
 export class HomeModule { }
